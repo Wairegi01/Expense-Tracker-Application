7247798e6aff1a6c5f4cde647b8199c708c50ab8
Wairegi01 <48497806+Wairegi01@users.noreply.github.com>
2025-01-08 23:59:49 +0100

    After Packages & EF_Models

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Type { get; set; } = "Expense";
+    }
+}
